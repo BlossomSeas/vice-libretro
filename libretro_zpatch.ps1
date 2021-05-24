39b8081ebd8bb1b523ea8cafea44a9faea26e17f
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-05-24 21:37:39 +0000

    Custom building

---
+++ libretro_zpatch.ps1
@@ -0,0 +1,13 @@
+git_merge libretro_option
+
+
+git_resolve color_palette_option both libretro/retro_setenv_front.h libretro/retro_getvars_front.h libretro/retro_getvars.c
+git_resolve aspect_ratio_option both libretro/retro_setenv_front.h libretro/retro_getvars_front.h
+
+
+git_resolve sid_model_option both libretro/retro_setenv_front.h libretro/retro_getvars_front.h
+git_resolve zoom_mode_option both libretro/retro_setenv_front.h libretro/retro_getvars_front.h
+git_resolve true_drive_option both libretro/retro_setenv_front.h libretro/retro_getvars_front.h
+
+
+git_resolve sid_volume_option both libretro/retro_setenv_back.h libretro/retro_getvars_front.h libretro/retro_getvars.c
